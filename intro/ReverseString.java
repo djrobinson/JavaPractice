91697e8013e79e4d087c97c4a8c9892f4dc0ee97
Danny Robinson <daniel@djrobinson.me>
2016-05-12 13:10:58 -0600

    Reverse string

---
+++ intro/ReverseString.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class ReverseString{
+
+  public static String reverse(String str){
+
+    String.word = args[0];
+    for (int i=word.length(); i >= 0; i--){
+      System.out.println(myList.get(i));
+      returnArr.add(myList.get(i));
+    }
+    return returnArr.get(0);
+  }
+
+  public static void main( String [] args ){
+    String printer = reverse("hello");
+    System.out.println(printer);
+
+  }
+}
